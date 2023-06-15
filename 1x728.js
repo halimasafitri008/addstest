8bcae09d3191f99c7782082beb4adfdd36135309
halimasafitri008 <120718024+halimasafitri008@users.noreply.github.com>
2023-06-15 15:54:18 +0700

    Create 1x728.js

---
+++ 1x728.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'e8b3b59f63c9a1c45d7d2e9323d0ebb9',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://cheapenleaving.com/e8b3b59f63c9a1c45d7d2e9323d0ebb9/invoke.js"></scr' + 'ipt>');
