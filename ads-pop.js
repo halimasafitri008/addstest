bdb3be8db979993197b2f55b5d46cab78ea905e5
halimasafitri008 <120718024+halimasafitri008@users.noreply.github.com>
2023-06-15 16:08:40 +0700

    Create ads-pop.js

---
+++ ads-pop.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '0ddc23d8ceda4dbfa343c6db3127e589',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://cheapenleaving.com/0ddc23d8ceda4dbfa343c6db3127e589/invoke.js"></scr' + 'ipt>');
